36c4c11827e575def9f7dca98f0aec9f33e45cdd
vanja97 <gavr97.97@mail.ru>
2020-10-21 22:59:27 +0300

    First coupon

--- cart/cart.py
+++ cart/cart.py
@@ -1,9 +1,28 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
 
 class Cart(object):
     ''' Class with respond without work cart with product'''
+
+    @property
+    def coupon(self):
+        ''''''
+        if self.coupon_id:
+            return Coupon.objects.get(id=self.coupon_id)
+        return None
+
+    def get_discount(self):
+        ''''''
+        if self.coupon:
+            return (self.coupon.discount / Decimal('100'))\
+                        * self.get_total_price()
+        return Decimal('0')
+
+    def get_total_price_after_discount(self):
+        ''''''
+        return self.get_total_price() - self.get_discount()
 
     def __init__(self, request):
         '''Inizialisation object cart'''
@@ -14,6 +33,8 @@ class Cart(object):
             # Save in session empty cart
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        # save coupon
+        self.coupon_id = self.session.get('coupon_id')
 
     def __iter__(self):
         '''Go cart and get objects Product'''
--- cart/views.py
+++ cart/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+from coupons.forms import CouponApplyForm
 
 
 @require_POST
@@ -35,7 +36,9 @@ def cart_detail(request):
             initial={'quantity': item['quantity'],
                      'update': True}
         )
+    coupon_apply_form = CouponApplyForm()
     return render(request, 'cart/detail.html',
-                  {'cart': cart})
+                  {'cart': cart,
+                   'coupon_apply_form':coupon_apply_form})
 
 # Create your views here.
