5a0721dceb3fe6dbb12559ab7557c5fd03d67752
vanja97 <gavr97.97@mail.ru>
2020-10-22 21:06:40 +0300

    Finish the book

--- belStock_shop/settings.py
+++ belStock_shop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -110,7 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+
+LANGUAGE_CODE = 'en-ru'
+
 
 TIME_ZONE = 'UTC'
 
@@ -134,3 +138,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID = 'cart'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 1
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
-
+from .recommender import Recommender
 
 def product_list(request, category_slug=None):
     '''Handler create page is list products and filter by categories'''
@@ -20,12 +20,13 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     '''Hander display page every product'''
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    print(request)
     cart_product_form = CartAddProductForm()
-    print('2 - detail.html - product_detail: ', product.name)
+    r = Recommender()
+    recommended_products = r.suggest_products_for([product], 4)
     return render(request, 'shop/product/detail.html',
                   {'product': product,
-                   'cart_product_form': cart_product_form})
+                   'cart_product_form': cart_product_form,
+                   'recommended_products': recommended_products})
 
 
 
